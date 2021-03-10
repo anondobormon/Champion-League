5aae04be0b23ae83659948af64ee1743c8675cbf
anondobormon <anondokpi@gmail.com>
2021-03-11 01:05:29 +0600

    added

--- src/Component/Teams/Teams.js
+++ src/Component/Teams/Teams.js
@@ -14,7 +14,7 @@ const Teams = (props) => {
         fetch(url)
             .then(res => res.json())
             .then(data => setLeague(data.leagues[0]))
-    }, [])
+    }, [idLeague])
     const { strBadge } = league;
     return (
         <div>
